c6f656e76da5da515892ec8c33ef08ee2cf54514
Ryan Amundson <ryanamundson1@yahoo.com>
2025-02-04 19:52:29 -0600

    made local version

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!bash
+uvicorn --host 0.0.0.0 main:app
--- webapp/main.py
+++ webapp/main.py
@@ -1,11 +1,15 @@
 import os
 import openai
+from openai import OpenAI
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import AzureSearch
-
+from qdrant_client import models, QdrantClient
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import kagglehub
 
 app = FastAPI()
 
@@ -14,13 +18,52 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_type = "azure"
 openai.api_version = "2023-05-15" 
 
-embeddings = OpenAIEmbeddings(deployment="demo-embedding", chunk_size=1)
+#embeddings = OpenAIEmbeddings(deployment="demo-embedding", chunk_size=1)
+# Get the data for embeddings
+# Download latest version
+path = kagglehub.dataset_download("cryptexcode/mpst-movie-plot-synopses-with-tags")
 
+print("Path to dataset files:", path)
+
+df = pd.read_csv(f'{path}/mpst_full_data.csv')
+df = df[df['plot_synopsis'].notna()] # remove any NaN values as it blows up serialization
+data = df.sample(1000).to_dict('records') # Get only 700 records. More records will make it slower to index
+len(data)
+
+qdrant = QdrantClient(":memory:") 
+
+# Create collection to store wines
+encoder = SentenceTransformer('all-MiniLM-L6-v2') 
+
+qdrant.recreate_collection(
+    collection_name="movie_plots",
+    vectors_config=models.VectorParams(
+        size=encoder.get_sentence_embedding_dimension(), # Vector size is defined by used model
+        distance=models.Distance.COSINE
+    )
+)
+
+qdrant.upload_points(
+    collection_name="movie_plots",
+    points=[
+        models.PointStruct(
+            id=idx,
+            vector=encoder.encode(doc["plot_synopsis"]).tolist(),
+            payload=doc,
+        ) for idx, doc in enumerate(data) # data is the variable holding all the wines
+    ]
+)
+
+
+client = OpenAI(
+    base_url="http://localhost:1234/v1", # "http://<Your api-server IP>:port"
+    api_key = "sk-no-key-required"
+)
 # Connect to Azure Cognitive Search
-acs = AzureSearch(azure_search_endpoint=os.getenv('SEARCH_SERVICE_NAME'),
-                 azure_search_key=os.getenv('SEARCH_API_KEY'),
-                 index_name=os.getenv('SEARCH_INDEX_NAME'),
-                 embedding_function=embeddings.embed_query)
+#acs = AzureSearch(azure_search_endpoint=os.getenv('SEARCH_SERVICE_NAME'),
+#                 azure_search_key=os.getenv('SEARCH_API_KEY'),
+#                 index_name=os.getenv('SEARCH_INDEX_NAME'),
+#                 embedding_function=embeddings.embed_query)
 
 class Body(BaseModel):
     query: str
@@ -47,11 +90,16 @@ def search(query):
     """
     Send the query to Azure Cognitive Search and return the top result
     """
-    docs = acs.similarity_search_with_relevance_scores(
-        query=query,
-        k=5,
+    #docs = acs.similarity_search_with_relevance_scores(
+    #    query=query,
+    #    k=5,
+    #)
+    #result = docs[0][0].page_content
+    result = qdrant.search(
+        collection_name="movie_plots",
+        query_vector=encoder.encode(query).tolist(),
+        limit=3
     )
-    result = docs[0][0].page_content
     print(result)
     return result
 
@@ -59,18 +107,22 @@ def search(query):
 def assistant(query, context):
     messages=[
         # Set the system characteristics for this chat bot
-        {"role": "system", "content": "Asisstant is a chatbot that helps you find the best wine for your taste."},
+        {"role": "system", "content": "Assistant is a chatbot that helps you find movies based on plots."},
 
         # Set the query so that the chatbot can respond to it
-        {"role": "user", "content": query},
+        {"role": "user", "content": str(query)},
 
         # Add the context from the vector search results so that the chatbot can use
         # it as part of the response for an augmented context
-        {"role": "assistant", "content": context}
+        {"role": "assistant", "content": str(context)}
     ]
 
-    response = openai.ChatCompletion.create(
-        engine="demo-alfredo",
-        messages=messages,
+    #response = openai.ChatCompletion.create(
+    #    engine="demo-alfredo",
+    #    messages=messages,
+    #)
+    completion = client.chat.completions.create(
+        model="LM_STUDIO_DEEPSEEK",
+        messages=messages
     )
-    return response['choices'][0]['message']['content']
+    return completion.choices[0].message
